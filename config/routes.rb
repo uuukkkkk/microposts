c41bca1509e68132c92389599503721f4e228cf9
uuukkkkk <usgwkig@gmail.com>
2015-09-21 10:07:53 +0000

    user-profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root to: 'static_pages#home'
   get 'signup', to:'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   
   resources :users
+  resources :sessions, only:[:new,:create,:destroy]
   
 end
