24e33a4ee1055f7b776964417e33b97172601596
uuukkkkk <usgwkig@gmail.com>
2015-09-25 12:05:48 +0000

    added profile area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -18,10 +19,26 @@ class UsersController < ApplicationController
     end
   end
 
-
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "edit done."
+      redirect_to @user
+    else
+      render 'edit'    
+    end
+  end
+  
   private
     
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation,:profile,:area)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
